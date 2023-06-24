2af657df0aa360c54db783bf1ae78452a24ddc0b
Saimon Kabir <126193249+SaimonKabirChowdhury@users.noreply.github.com>
2023-06-25 00:44:25 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express);
+const request = require("request);
+const app = express();
+
+app.listen(3000,function(){
+  console.log("Ok);
+});
+                        
+                        
