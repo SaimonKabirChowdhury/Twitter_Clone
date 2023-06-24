a7c2ab219c51915eb5117c347f8efdc5eddf685b
Saimon Kabir <126193249+SaimonKabirChowdhury@users.noreply.github.com>
2023-06-25 02:25:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const request = require("request");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.listen(3000,function(){
   console.log("Ok");
 });
+app.use(bodyParser.urlencoded{extended:true});
                         
                         
 app.get('/',function(req,res){
