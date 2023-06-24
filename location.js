bdae0dcc1496cefe55eed68979cea49d2aaa0506
Saimon Kabir <126193249+SaimonKabirChowdhury@users.noreply.github.com>
2023-06-25 03:25:35 +0600

    Update location.js

--- location.js
+++ location.js
@@ -1,1 +1,25 @@
+function getLocation async(){
+var x = document.getElementById("demo");
+if (navigator.geolocation){
+    
+    
+    navigator.geolocation.getCurrentPosition(showpostion);
+    
+    
+    
+    
+    
+}else{
+    
+    x.innerHTML = "Geolocation is not supported by this browser.";
+    
+    
+    }
 
+
+
+
+}function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
