894194d47d6bfd427bacddd8212158b8ce0f1915
Saimon Kabir <126193249+SaimonKabirChowdhury@users.noreply.github.com>
2023-06-25 00:47:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express);
-const request = require("request);
+const express = require("express");
+const request = require("request");
 const app = express();
 
 app.listen(3000,function(){
